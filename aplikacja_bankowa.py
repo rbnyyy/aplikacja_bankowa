b564ff017506df9d1890bd3f66cd37a7054208ed
rbnyyy <109953278+rbnyyy@users.noreply.github.com>
2024-03-26 20:48:03 +0100

    Update aplikacja_bankowa.py
    correct ver

--- aplikacja_bankowa.py
+++ aplikacja_bankowa.py
@@ -144,26 +144,3 @@ while zaczynamy == "logowanie":
         weryfikacja_logowanie()
     elif wejscie == "nie":
         sys.exit()
-
-
-def check():
-    while haslo_potwierdzenie != haslo:
-        print("Hasla nie są zgodne")
-        password()
-
-
-print("CZEŚĆ :)\nWitamy Cię w aplikacji do zarządzania walutami Currverter")
-
-rozpoczecie = str(input("Czy chcesz założyć nowe konto walutowe? TAK/NIE")).lower()
-
-
-if rozpoczecie == "tak":
-    login = str(input("Podaj swoj login do rejestracji."))
-else:
-    print("Niepoprawny login")
-
-if len(login) > 0:
-    password()
-    check()
-    zapis_danych()
-
